16ad7f6d53e376faba5d463d9a31da5b8206655a
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2022-10-10 13:22:18 +0200

    Add MoviesList and Detail Views

--- TheMovieApp/TheMovieApp/Views/MoviesListView.swift
+++ TheMovieApp/TheMovieApp/Views/MoviesListView.swift
@@ -8,22 +8,49 @@
 import SwiftUI
 
 struct MoviesListView: View {
+    struct NewsItem: Decodable, Identifiable {
+        let id: Int
+        let title: String
+        let strap: String
+    }
+
+    @State private var movies = 10
     var body: some View {
-        ZStack {
-            List(1..<100) { row in
-                MovieRowView()
-                    .listRowBackground(Color.red)
+        NavigationStack {
+            List {
+                ForEach(0..<movies, id:\.self) { row in
+                    ZStack {
+                        NavigationLink {
+                            MovieDetailView()
+                        } label: {
+                            EmptyView()
+                        }
+                        MovieRowView()
+                    }
+                    .listRowSeparator(.hidden)
+                    .listRowBackground(Color.background)
+                }
             }
             .listStyle(.plain)
+            .scrollContentBackground(.hidden)
+            .background(Color.background)
             .refreshable {
-                print("Do your refresh work here")
+                do {
+                    let url = URL(string: "https://www.hackingwithswift.com/samples/news-1.json")!
+                    let (data, _) = try await URLSession.shared.data(from: url)
+                    movies += try JSONDecoder().decode([NewsItem].self, from: data).count / 2
+                } catch {}
             }
+            .tint(.red)
+            .toolbar(.hidden)
         }
     }
 }
 
+#if DEBUG
 struct MoviesListView_Previews: PreviewProvider {
     static var previews: some View {
         MoviesListView()
     }
 }
+#endif
