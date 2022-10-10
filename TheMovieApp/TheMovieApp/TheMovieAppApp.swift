04a0728c6a3b2ff6c419d38951216e56172b1c55
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2022-10-10 10:13:28 +0200

    Add Initial Project and custom colors

---
+++ TheMovieApp/TheMovieApp/TheMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieAppApp.swift
+//  TheMovieApp
+//
+//  Created by Cédric Bahirwe on 09/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
